6d8f47365d1b8248a8e841f7e93cf83e1bd2076d
lucas <lucas.rodrigues@neocredit.com.br>
2025-01-06 22:08:50 -0300

    identação e while

--- 100_dias_de_python/day_6.py
+++ 100_dias_de_python/day_6.py
@@ -1,5 +1,35 @@
-def my_function():
-    print("Hello")
-    print("Bye")
+def my_function(count): 
+    print("---")
+    if count == 0:
+        print("It's true")
 
+count = 0
+while count < 5:
+    print(count)
+    my_function(count)
+    count += 1
 
+count = 0
+while count < 10:
+    if count % 2 == 0:
+        print(f"{count} é par")
+    else:
+        print(f"{count} é ímpar")
+    count += 1
+    
+count = 0
+while count < 10:
+    count += 1
+    if count == 5:
+        continue  # Pula o resto do loop quando count é 5
+    if count == 8:
+        break  # Sai do loop quando count é 8
+    print(count)
+    
+outer_count = 0
+while outer_count < 3:
+    inner_count = 0
+    while inner_count < 3:
+        print(f"outer_count: {outer_count}, inner_count: {inner_count}")
+        inner_count += 1
+    outer_count += 1
